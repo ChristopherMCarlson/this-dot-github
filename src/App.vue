91ce647c180745202dfb40af5658393420d9f074
ChristopherMCarlson <41597047+ChristopherMCarlson@users.noreply.github.com>
2021-04-19 21:53:27 -0600

    EOD

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      dark
+      flat
+    >
+    <v-icon large>mdi-github</v-icon>
+      <v-avatar
+        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+        size="32"
+      ></v-avatar>
+      <v-btn
+        text
+        small
+        v-for="link in links"
+        :key="link"
+      >
+        {{ link }}
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row class='justify-space-between'>
+          <v-col cols='12' class='text-center'>
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Pull Requests',
+        'Issues',
+        'Marketplace',
+        'Explore',
+      ],
+    }),
+  }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container height='80vh'>
+    <v-row class='justify-space-around mt-5' align='center'>
+      <v-col cols='8' class='text-start'>
+        <h2>
+          <v-icon>mdi-magnify</v-icon> Search more than 553M issues
+        </h2>
+      </v-col>
+      <v-col cols='8' class='text-center'>
+        <v-row class='justify-center'>
+          <v-col cols='11'>
+            <v-text-field
+              label="Search Github"
+              placeholder="Search Github"
+              single-line
+              outlined
+            ></v-text-field>
+          </v-col>
+          <v-col cols='1'>
+            <v-btn min-height='56px'>Search</v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
