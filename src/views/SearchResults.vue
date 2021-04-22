94f44e3f3f5f016f406977590cb2bc375851d449
ChristopherMCarlson <41597047+ChristopherMCarlson@users.noreply.github.com>
2021-04-21 23:06:22 -0600

    EOD Styling

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -1,8 +1,26 @@
 <template>
   <v-container height='80vh'>
     <v-row class='justify-space-around mt-5' align='center'>
+      <v-col cols='8' class='text-start'>
+        <p>{{searchResults.total_count}} user(s)</p>
+      </v-col>
       <v-col cols='8' class='text-center' v-for='result in searchResults.items' :key="result.id">
-        <p>{{result.html_url}}</p>
+        <v-row>
+          <v-col cols='1'>
+            <v-avatar size="36">
+              <img
+                :src="result.avatar_url"
+                alt="John"
+              >
+            </v-avatar>
+          </v-col>
+          <v-col cols='10' class='text-start'>
+            <a :href='result.html_url'>{{result.login}}</a>
+          </v-col>
+          <v-col cols='1' class='text-center'>
+            <v-btn>Follow</v-btn>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
